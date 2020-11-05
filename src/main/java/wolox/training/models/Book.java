b0e16a1b6344b7b842b35909aaadbdb83720e1b0
Pablo Andres Hincapie <pabloandreshincapie@Woloxs-MacBook-Pro.local>
2020-11-05 09:28:31 -0500

    I change the way of auto-incremental

--- src/main/java/wolox/training/models/Book.java
+++ src/main/java/wolox/training/models/Book.java
@@ -6,12 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 @Entity
 public class Book {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "BOOK_SEQ")
+    @SequenceGenerator(name = "BOOK_SEQ", sequenceName ="BOOK_SEQ")
     private long id;
     @Column
     private String genre;
