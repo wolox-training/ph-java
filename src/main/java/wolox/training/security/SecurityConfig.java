fdc7e8faa99d239c8f6110c4b793d1f19a18d6d8
Pablo Andres Hincapie <pabloandreshincapie@Woloxs-MacBook-Pro.local>
2020-11-19 12:13:23 -0500

    I added files and settings to spring security

---
+++ src/main/java/wolox/training/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package wolox.training.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private CustomAuthenticationProvider authProvider;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authProvider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/users", "/api/books").permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic()
+                .and()
+                .csrf().disable();
+    }
+
+}
