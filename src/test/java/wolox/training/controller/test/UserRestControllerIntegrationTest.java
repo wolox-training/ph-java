ebbe9dfcca5b53e70ca883fb4ac99cbd012cb0b3
Pablo Andres Hincapie <pabloandreshincapie@Woloxs-MacBook-Pro.local>
2020-11-19 08:22:40 -0500

    I added changes to

--- src/test/java/wolox/training/controller/test/UserRestControllerIntegrationTest.java
+++ src/test/java/wolox/training/controller/test/UserRestControllerIntegrationTest.java
@@ -1,19 +1,6 @@
 package wolox.training.controller.test;
 
-import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
-import static java.util.Collections.get;
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static sun.nio.ch.Reflect.get;
 
-
-import com.sun.javafx.tools.ant.Application;
-import java.util.Arrays;
-import java.util.List;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -25,12 +12,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import wolox.training.controllers.UserController;
 import wolox.training.models.User;
 
-@RunWith(SpringRunner.class)
+
 @WebMvcTest(UserController.class)
 
-@SpringBootTest(
-        SpringBootTest.WebEnvironment.MOCK,
-        classes = Application.class)
 @AutoConfigureMockMvc
 @TestPropertySource(
         locations = "classpath:application-integrationtest.properties")
@@ -38,32 +22,6 @@ public class UserRestControllerIntegrationTest {
     @Autowired
     private MockMvc mvc;
 
-    @Test
-    public void givenUser_whenGetUser_thenReturnJsonArray()
-            throws Exception {
 
-        User pablo = new User("pablo");
 
-        List<User> allEmployees = Arrays.asList(pablo);
-
-        mvc.perform(get("/api/user")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].name", is(pablo.getName())));
-    }
-
-    @Test
-    public void givenUser_whenGetUser_thenStatus200()
-            throws Exception {
-
-       // createTestUser("pablo");
-
-        mvc.perform(get("/api/user")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content()
-                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].name", is("pablo")));
-    }
 }
--- src/test/java/wolox/training/model/test/UserModelIntegrationTest.java
+++ src/test/java/wolox/training/model/test/UserModelIntegrationTest.java
@@ -1,14 +1,18 @@
 package wolox.training.model.test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
+import wolox.training.exceptions.BookNotFoundException;
 import wolox.training.models.User;
 import wolox.training.repositories.UserRepository;
 
@@ -24,7 +28,7 @@ public class UserModelIntegrationTest {
 
 
     @Test
-    public void whenFindByName_thenReturnBook() {
+    public void whenFindByName_thenReturnUser() {
         // given
         User pablo = new User("pablo");
         entityManager.persist(pablo);
@@ -38,11 +42,13 @@ public class UserModelIntegrationTest {
                 .isEqualTo(pablo.getName());
     }
 
-    @Before
+    @BeforeAll
     public void setUp() {
         User pablo = new User("pablo");
 
         Mockito.when(userRepository.findByName(pablo.getName()))
                 .thenReturn(pablo);
     }
+
+
 }
