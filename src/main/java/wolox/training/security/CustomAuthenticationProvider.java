b8af7733ad5e9f8be842c0fa3416c9415f96cea1
Pablo Andres Hincapie <pabloandreshincapie@Woloxs-MacBook-Pro.local>
2020-11-19 13:47:39 -0500

    I added methods for security

--- src/main/java/wolox/training/security/CustomAuthenticationProvider.java
+++ src/main/java/wolox/training/security/CustomAuthenticationProvider.java
@@ -3,10 +3,13 @@ package wolox.training.security;
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import wolox.training.repositories.UserRepository;
 
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
@@ -14,25 +17,22 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public Authentication authenticate(Authentication authentication)
             throws AuthenticationException {
 
         String name = authentication.getName();
         String password = authentication.getCredentials().toString();
-
-        if (shouldAuthenticateAgainstThirdPartySystem()) {
-
-            // use the credentials
-            // and authenticate against the third-party system
-            return new UsernamePasswordAuthenticationToken(
-                    name, password, new ArrayList<>());
-        } else {
-            return null;
-        }
+        userRepository.findUserByUsername(name).filter(user -> passwordEncoder.matches(password, password))
+        .orElseThrow(()-> new BadCredentialsException("Incorrect Credential"));
+      return new UsernamePasswordAuthenticationToken(name, password, new ArrayList<>());
     }
     @Override
-    public boolean supports(Class<?> authentication) {
+    public boolean supports(Class<?> authentication){
         return authentication.equals(UsernamePasswordAuthenticationToken.class);
     }
 }
