29128d9ee7dd4daf577d3c9d0de9e302600ad3e8
Pablo Andres Hincapie <pabloandreshincapie@Woloxs-MacBook-Pro.local>
2020-11-25 10:18:34 -0500

    I added settings to security

--- src/main/java/wolox/training/security/SecurityConfig.java
+++ src/main/java/wolox/training/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -27,9 +28,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/api/users", "/api/books").permitAll()
                 .anyRequest().authenticated()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().httpBasic()
                 .and()
                 .csrf().disable();
+
+
     }
 
 }
